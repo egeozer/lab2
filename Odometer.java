0a165a6027635a446db881f2ce143048dc442d8e
mtk3 <mtk3@users.noreply.github.com>
2017-01-21 14:02:43 -0500

    Add files via upload

---
+++ Odometer.java
@@ -0,0 +1,196 @@
+/*
+ * Odometer.java
+ */
+
+package ev3Odometer;
+
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+
+public class Odometer extends Thread {
+	// robot position
+	private double x, y, theta;
+	private int leftMotorTachoCount, rightMotorTachoCount;
+	private EV3LargeRegulatedMotor leftMotor, rightMotor;
+	// odometer update period, in ms
+	private static final long ODOMETER_PERIOD = 25;
+
+	// lock object for mutual exclusion
+	private Object lock;
+	
+	
+	// private variables for odometry measurement
+	private double WR, WB;
+	private double distance, angle;		//distance in cm, angle in radians
+	private double prevDistance, prevAngle;
+
+	// default constructor
+	public Odometer(EV3LargeRegulatedMotor leftMotor,EV3LargeRegulatedMotor rightMotor) {
+		this.leftMotor = leftMotor;
+		this.rightMotor = rightMotor;
+		this.x = 0.0;
+		this.y = 0.0;
+		this.theta = 0.0;
+		this.leftMotorTachoCount = 0;
+		this.rightMotorTachoCount = 0;
+		lock = new Object();
+	}
+
+	// run method (required for Thread)
+	public void run() {
+		long updateStart, updateEnd;
+
+		while (true) {
+			updateStart = System.currentTimeMillis();
+			//TODO put (some of) your odometer code here
+			//get tachometer values
+			leftMotorTachoCount = leftMotor.getTachoCount();
+			rightMotorTachoCount = rightMotor.getTachoCount();
+			
+			//calculate distance and heading
+			distance = WR * Math.PI / 360 * ( leftMotorTachoCount + rightMotorTachoCount);
+			angle = WR / WB * Math.PI / 180 * ( leftMotorTachoCount - rightMotorTachoCount);
+			
+			distance -= prevDistance;
+			angle -= prevAngle;
+			
+			//update previous values
+			prevDistance += distance;
+			prevAngle += angle;
+
+			synchronized (lock) {
+				/**
+				 * Don't use the variables x, y, or theta anywhere but here!
+				 * Only update the values of x, y, and theta in this block. 
+				 * Do not perform complex math
+				 * 
+				 */
+				//Set variables theta = -0.7376; //TODO replace example value
+				
+				theta += angle;
+				if(theta < 0)
+					theta = Math.abs(theta);
+
+				x += distance * Math.cos(theta);
+				y += distance * Math.sin(theta);
+			}
+
+			// this ensures that the odometer only runs once every period
+			updateEnd = System.currentTimeMillis();
+			if (updateEnd - updateStart < ODOMETER_PERIOD) {
+				try {
+					Thread.sleep(ODOMETER_PERIOD - (updateEnd - updateStart));
+				} catch (InterruptedException e) {
+					// there is nothing to be done here because it is not
+					// expected that the odometer will be interrupted by
+					// another thread
+				}
+			}
+		}
+	}
+
+	// accessors
+	public void getPosition(double[] position, boolean[] update) {
+		// ensure that the values don't change while the odometer is running
+		synchronized (lock) {
+			if (update[0])
+				position[0] = x;
+			if (update[1])
+				position[1] = y;
+			if (update[2])
+				position[2] = theta;
+		}
+	}
+
+	public double getX() {
+		double result;
+
+		synchronized (lock) {
+			result = x;
+		}
+
+		return result;
+	}
+
+	public double getY() {
+		double result;
+
+		synchronized (lock) {
+			result = y;
+		}
+
+		return result;
+	}
+
+	public double getTheta() {
+		double result;
+
+		synchronized (lock) {
+			result = theta;
+		}
+
+		return result;
+	}
+
+	// mutators
+	public void setPosition(double[] position, boolean[] update) {
+		// ensure that the values don't change while the odometer is running
+		synchronized (lock) {
+			if (update[0])
+				x = position[0];
+			if (update[1])
+				y = position[1];
+			if (update[2])
+				theta = position[2];
+		}
+	}
+
+	public void setX(double x) {
+		synchronized (lock) {
+			this.x = x;
+		}
+	}
+
+	public void setY(double y) {
+		synchronized (lock) {
+			this.y = y;
+		}
+	}
+
+	public void setTheta(double theta) {
+		synchronized (lock) {
+			this.theta = theta;
+		}
+	}
+
+	/**
+	 * @return the leftMotorTachoCount
+	 */
+	public int getLeftMotorTachoCount() {
+		return leftMotorTachoCount;
+	}
+
+	/**
+	 * @param leftMotorTachoCount the leftMotorTachoCount to set
+	 */
+	public void setLeftMotorTachoCount(int leftMotorTachoCount) {
+		synchronized (lock) {
+			this.leftMotorTachoCount = leftMotorTachoCount;	
+		}
+	}
+
+	/**
+	 * @return the rightMotorTachoCount
+	 */
+	public int getRightMotorTachoCount() {
+		return rightMotorTachoCount;
+	}
+
+	/**
+	 * @param rightMotorTachoCount the rightMotorTachoCount to set
+	 */
+	public void setRightMotorTachoCount(int rightMotorTachoCount) {
+		synchronized (lock) {
+			this.rightMotorTachoCount = rightMotorTachoCount;	
+		}
+	}
+}
